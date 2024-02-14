60e40611666e9762e266e9dddffdd1e7cfb24f9f
Ayush Asthana <iamayushasthana@gmail.com>
2024-02-14 15:26:21 +0530

    final changes for v1.0

--- main.py
+++ main.py
@@ -11,25 +11,37 @@ from resume.extract_names import extract_names
 from resume.extract_phone_number import extract_phone_number
 from resume.extract_emails import extract_emails
 from resume.extract_skills import extract_skills
-from resume.extract_education import extract_education
+
+ROWS_PER_WORKSHEET = 45000
 
 def main():
   PATH = 'test_resumes'
   files = get_files_list(PATH)
-  [output, output_sheet] = generate_output_file()
+  workbook = generate_workbook()
+  worksheet = generate_worksheet(workbook)
 
+  row_number = 1 
   for index, file in enumerate(files):
     text = extract_text(file, PATH)
     source = file[1]
     names = extract_names(text)
     phone_number = extract_phone_number(text)
     emails = join_to_string(extract_emails(text))
-    skills = join_to_string(extract_skills(text))
-    education = join_to_string(extract_education(text))
 
-    write_to_output_sheet(output_sheet, index, source, names, phone_number, emails, skills, education)
+    skills = extract_skills(text)
+    skills_count = len(skills)
 
-  output.close()
+    if (skills_count + row_number > ROWS_PER_WORKSHEET):
+      worksheet = generate_worksheet(workbook)
+      row_number = 1 
+
+    for skill in skills:
+      row_number = write_to_worksheet(worksheet, row_number, source, names, phone_number, emails, skill)
+    print(f"Successfully extracted data from {source}!!")
+
+  print("Saving data to excel file!!")
+  workbook.close()
+  print("Data extracted successfully!!")
 
 
 def get_files_list(path):
@@ -46,28 +58,33 @@ def extract_text(file_data, path):
       text = extract_text_from_pdf(path + '/' + file_data[1])
   return text
 
-def generate_output_file():
+def generate_workbook():
   workbook = xlsxwriter.Workbook(str(time()) + '.xlsx')
+  return workbook
+
+def generate_worksheet(workbook):
   worksheet = workbook.add_worksheet()
-  return [workbook, worksheet]
+  return worksheet
 
-def write_to_output_sheet(output_sheet, index, source, names, phone_number, emails, skills, education):
-  if (index == 0):
-    row = str(index + 1)
-    output_sheet.write('A' + row, "source")
-    output_sheet.write('B' + row, "names")
-    output_sheet.write('C' + row, "phone_number")
-    output_sheet.write('D' + row, "emails")
-    output_sheet.write('E' + row, "skills")
-    output_sheet.write('F' + row, "education")
+def write_to_worksheet(worksheet, row_number, source, name, phone_number, emails, skill):
+  if (row_number == 1):
+    row = str(row_number)
+    worksheet.write('A' + row, "Source")
+    worksheet.write('B' + row, "Name")
+    worksheet.write('C' + row, "Phone Number")
+    worksheet.write('D' + row, "Emails")
+    worksheet.write('E' + row, "Skills")
+    row_number += 1
 
-  row = str(index + 2)
-  output_sheet.write('A' + row, source)
-  output_sheet.write('B' + row, names)
-  output_sheet.write('C' + row, phone_number)
-  output_sheet.write('D' + row, emails)
-  output_sheet.write('E' + row, skills)
-  output_sheet.write('F' + row, education)
+  row = str(row_number)
+  worksheet.write('A' + row, source)
+  worksheet.write('B' + row, name)
+  worksheet.write('C' + row, phone_number)
+  worksheet.write('D' + row, emails)
+  worksheet.write('E' + row, skill)
+  row_number += 1
+
+  return row_number
 
 def join_to_string(list):
   return " ; ".join(list)
